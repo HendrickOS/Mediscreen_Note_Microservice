000898e983cdcddf98ec98b0fcd6b424ff11dd9d
Hendrick Osseux <osseux.hendrick@gmail.com>
2022-12-28 14:11:40 +0100

    Fist commit

---
+++ src/main/java/com/project9/Mediscreen_Note_Microservice/MediscreenNoteMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.project9.Mediscreen_Note_Microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediscreenNoteMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediscreenNoteMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project9/Mediscreen_Note_Microservice/MediscreenNoteMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project9.Mediscreen_Note_Microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediscreenNoteMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
