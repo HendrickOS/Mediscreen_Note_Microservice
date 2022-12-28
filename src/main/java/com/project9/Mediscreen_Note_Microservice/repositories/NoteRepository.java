e58eb3ffde3938f398d4914101203aea8e788973
Hendrick Osseux <osseux.hendrick@gmail.com>
2022-12-28 18:04:31 +0100

    Add Template

--- src/main/java/com/project9/Mediscreen_Note_Microservice/repositories/NoteRepository.java
+++ src/main/java/com/project9/Mediscreen_Note_Microservice/repositories/NoteRepository.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.CrudRepository;
 
+import com.project9.Mediscreen_Note_Microservice.domain.Note;
+
 public interface NoteRepository extends MongoRepository<Note, Integer>, CrudRepository<Note, Integer> {
 
 	public List<Note> findByPatientsName(String patientsName);
 
 	public Note save(Note note);
 
-	public void deleteById(Integer id);{
+	public void deleteById(Integer id);
 
 }
