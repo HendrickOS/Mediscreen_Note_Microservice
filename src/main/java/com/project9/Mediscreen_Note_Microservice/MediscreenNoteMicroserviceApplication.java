8f4ef2e49459e1700d9bd4a54d4b9a41b26be7f3
Hendrick Osseux <osseux.hendrick@gmail.com>
2022-12-29 22:48:03 +0100

    Transform into eurekaclient to connect eurekaserver

--- src/main/java/com/project9/Mediscreen_Note_Microservice/MediscreenNoteMicroserviceApplication.java
+++ src/main/java/com/project9/Mediscreen_Note_Microservice/MediscreenNoteMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.project9.Mediscreen_Note_Microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MediscreenNoteMicroserviceApplication {
 
 	public static void main(String[] args) {
