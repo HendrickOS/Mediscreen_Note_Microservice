411dcc0d98264cefd84d307fffcd9e5025557804
Hendrick Osseux <osseux.hendrick@gmail.com>
2023-01-27 15:12:17 +0100

    ADD Tests & JUnit+Mockito dependency

---
+++ src/test/java/com/project9/Mediscreen_Note_Microservice/NoteServiceTest.java
@@ -0,0 +1,41 @@
+package com.project9.Mediscreen_Note_Microservice;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.project9.Mediscreen_Note_Microservice.domain.Note;
+import com.project9.Mediscreen_Note_Microservice.repositories.NoteRepository;
+import com.project9.Mediscreen_Note_Microservice.services.NoteService;
+
+@SpringBootTest
+public class NoteServiceTest {
+
+	@Autowired
+	private NoteService noteServiceUnderTest;
+	@MockBean
+	private NoteRepository noteRepositoryMocked;
+
+	@Test
+	public void findNoteByPatientsName() {
+		String givenPatientName = "fullname";
+		List<Note> expected = new ArrayList<Note>();
+
+		when(noteRepositoryMocked.findByPatientsName(givenPatientName)).thenReturn(expected);
+
+		List<Note> result = noteServiceUnderTest.findByPatientsName(givenPatientName);
+
+		assertThat(result).isEqualTo(expected);
+		verify(noteRepositoryMocked, times(1)).findByPatientsName(givenPatientName);
+	}
+
+}
